09b3de4d53cabf7e5a46fef71e02843d847fcade
cwtu <cwtu12@gmail.com>
2022-03-23 21:02:46 -0400

    dummy data

---
+++ src/assets/data/data.tsx
@@ -0,0 +1,38 @@
+export const experiences : BlockProp[] = [
+	{
+		title: "Experience 1",
+		description: "this is my expereince",
+		time: "feb. 2021"
+	},
+	{
+		title: "Experience 2",
+		description: "this is my expereince",
+		time: "sep. 2021"
+	}
+]
+
+export const courses : BlockProp[] = [
+	{
+		title: "Course 1",
+		description: "this is my course",
+		time: "feb. 2021"
+	},
+	{
+		title: "Course 2",
+		description: "this is my course",
+		time: "sep. 2021"
+	}
+]
+
+export const projects : BlockProp[] = [
+	{
+		title: "Project 1",
+		description: "this is my project",
+		time: "feb. 2021"
+	},
+	{
+		title: "Project 2",
+		description: "this is my project",
+		time: "sep. 2021"
+	}
+]
